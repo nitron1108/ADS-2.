2647199fd5a054f6f3988c15c8fbf03c581cd74e
nitron1108 <mja29122@gmail.com>
2026-04-05 23:51:18 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,53 @@
 
 
 double pown(double value, uint16_t n) {
-  return 0.0;
+  double res = 1.0;
+  for (uint16_t i = 0; i < n; i++) {
+    res *= value;
+  }
+  return res;
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+  uint64_t res = 1;
+  for (uint16_t i = 2; i <= n; i++) {
+    res *= i;
+  }
+  return res;
 }
 
-double calcItem(double x, uint16_t n) {
-  return 0.0;
-}
+double calcItem(double x, uint16_t n) { return pown(x, n) / fact(n); }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+  double t = 0.0;
+  for (uint16_t i = 0; i <= count; i++) {
+    t += calcItem(x, i);
+  }
+  return t;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+    double t = 0.0;
+    for (uint16_t n = 0; n < count; n++) {
+    double item = pown(x, 2 * n + 1) / fact(2 * n + 1);
+    if (n % 2 == 0) {
+      t += item;
+    } else {
+      t -= item;
+    }
+  }
+  return t;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+  double t = 0.0;
+  for (uint16_t n = 0; n < count; n++) {
+    double item = pown(x, 2 * n) / fact(2 * n);
+    if (n % 2 == 0) {
+      t += item;
+    } else {
+      t -= item;
+    }
+  }
+  return t;
 }
