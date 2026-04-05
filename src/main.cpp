c6c9298cdb214ead4f716d9bbfef5233ea0464ac
nitron1108 <mja29122@gmail.com>
2026-04-05 23:53:03 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,11 @@
 #include "alg.h"
 
 int main() {
-    return 0;
+  double val = 1.0;
+  uint16_t steps = 10;
+  std::cout << "Test Taylor for x = " << val << std::endl;
+  std::cout << "Exp: " << expn(val, steps) << std::endl;
+  std::cout << "Sin: " << sinn(val, steps) << std::endl;
+  std::cout << "Cos: " << cosn(val, steps) << std::endl;
+  return 0;
 }
